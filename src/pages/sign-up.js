49b9dc0d1b98ddc09487ac526a1c4d0e9be892a6
Jason Pallone <50902289+Jason-Pallone@users.noreply.github.com>
2022-06-09 17:10:16 -0400

    Setting up sign up page

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,33 +1,58 @@
 import { useState, useContext, useEffect } from "react";
 import {  useNavigate, Link } from "react-router-dom";
 import FirebaseContext from '../context/firebase';
+import doesUsernameExist from "../services/firebase";
 import * as ROUTES from '../constants/routes'
 
-export default function Login () {
+export default function SignUp () {
     const navigate = useNavigate();
     const { firebase } = useContext(FirebaseContext);
-
+    
+    const [userName, setUserName ] = useState('');
+    const [fullName, setFullName ] = useState('');
     const [emailAddress, setEmailAddress ] = useState('');
     const [password, setPassword ] = useState('');
+
  
     const [error, setError ] = useState('');
     const isInvalid = password === '' || emailAddress === '';
 
-    const handleLogin = async (event) => {
+    const handleSignUp = async (event) => {
       event.preventDefault();
 
-      try {
-        await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-        navigate(ROUTES.DASHBOARD);
-      } catch (error) {
-        setEmailAddress('');
-        setPassword('');
-        setError(error.message);
+      const usernameExists = await doesUsernameExist(userName)
+
+      if (!usernameExists) {
+        try {
+          const createdUserResult = await firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+
+          await createdUserResult.user.updateProfile({
+            displayName: userName
+          });
+
+          await firebase.firestore().collection('users').add({
+            userId: createdUserResult.user.uid,
+            username: userName.toLowerCase(),
+            fullName,
+            emailAddress: emailAddress.toLowerCase(),
+            following: [],
+            dateCreated: Date.now()
+          });
+
+          navigate(ROUTES.DASHBOARD)
+        } catch (error) {
+          let errorMessage = error.message.replace('Firebase:','');
+          setError(`Hey ${fullName}! Sorry but ${errorMessage.toLowerCase()}`);
+        }
+      } else {
+          setError(`Hey ${fullName}! Sorry but that username is already taken, please try another one.`)
       }
     };
 
     useEffect(() => {
-        document.title = 'Login - Instagram'
+        document.title = 'Sign Up - Instagram'
     }, []);
 
     return (
@@ -43,7 +68,23 @@ export default function Login () {
 
                 {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-                <form onSubmit={handleLogin} method="POST">
+                <form onSubmit={handleSignUp} method="POST">
+                    <input
+                      aria-label="Enter your username"
+                      type="text"
+                      placeholder="Username"
+                      className="text-sm text-gray-base w-full mr-2 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+                      onChange={({target}) => setUserName(target.value)}
+                      value={userName}
+                    />
+                    <input
+                      aria-label="Enter your full name"
+                      type="text"
+                      placeholder="Full Name"
+                      className="text-sm text-gray-base w-full mr-2 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+                      onChange={({target}) => setFullName(target.value)}
+                      value={fullName}
+                    />
                     <input
                       aria-label="Enter your email address"
                       type="text"
@@ -71,9 +112,9 @@ export default function Login () {
                 </form>
             </div>
             <div className="flex justify-center items-center flex-col w-full rounded bg-white p-4 border border-grey-primary">
-                <p className="text-sm">Don't have an account?{' '}
-                  <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-                    Sign up
+                <p className="text-sm">Have an account?{' '}
+                  <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+                    Login
                   </Link>
                 </p>
             </div>
